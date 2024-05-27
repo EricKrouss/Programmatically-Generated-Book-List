577b8d17164c4307436a6bf345625176e8db2514
Eric <eric@krouss.net>
2024-05-27 15:45:17 -0400

    Converted project to React 16

--- books.js
+++ books.js
@@ -1,44 +1,34 @@
-class Book extends HTMLElement{
-    constructor(){
-        super();
-    }
-    set book(book){
-        this.innerHTML = `
-            <div class="card">
-              <h5 class="card-header">${book.title}</h5>
-              <div class="card-body">
-                <h5>${book.subtitle}</h5>
-                    <p class="card-text">
-                    <table class="table">
+function Book({data}){
+    return (
+            <div className="card">
+              <h5 className="card-header">{data.title}</h5>
+              <div className="card-body">
+                <h5>{data.subtitle}</h5>
+                    <table className="table">
+                        <tbody>
                         <tr>
-                            <td class="text-success font-weight-bold">Title:</td>
-                            <td>${book.title}</td>
+                            <td className="text-success font-weight-bold">Title:</td>
+                            <td>{data.title}</td>
                         </tr>
                         <tr>
-                            <td class="text-success font-weight-bold">Subtitle:</td>
-                            <td>${book.subtitle}</td>
+                            <td className="text-success font-weight-bold">Subtitle:</td>
+                            <td>{data.subtitle}</td>
                         </tr>
                         <tr>
-                            <td class="text-success font-weight-bold">Author:</td>
-                            <td>${book.author}</td>
+                            <td className="text-success font-weight-bold">Author:</td>
+                            <td>{data.author}</td>
                         </tr>
                         <tr>
-                            <td class="text-success font-weight-bold">Publisher:</td>
-                            <td>${book.publisher}</td>
+                            <td className="text-success font-weight-bold">Publisher:</td>
+                            <td>{data.publisher}</td>
                         </tr>
                         <tr>
-                            <td class="text-success font-weight-bold">Description:</td>
-                            <td>${book.description}</td>
+                            <td className="text-success font-weight-bold">Description:</td>
+                            <td>{data.description}</td>
                         </tr>
+                        </tbody>
                     </table>
-                    </p>
               </div>
             </div>
-        `;
+        );
     }
-}
-
-
-        
-
-customElements.define('mit-book', Book);
